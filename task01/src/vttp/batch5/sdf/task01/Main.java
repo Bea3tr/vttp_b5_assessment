793caa0689231867eaabe26517d23cf09af65f5c
Beatrice <beatricesim31@gmail.com>
2024-10-30 13:38:10 +0800

    changed sum of cyclists

--- task01/src/vttp/batch5/sdf/task01/Main.java
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -5,9 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import vttp.batch5.sdf.task01.models.BikeEntry;
@@ -36,16 +34,16 @@ public class Main {
 				entries.add(entry);
 			}
 			br.close();
-			// Find top 5 days with most cyclist
+
+			// Find top 5 days with most cyclist (casual + registered)
 			List<BikeEntry> sorted = entries.stream()
-					.sorted((e1, e2) -> Integer.compare(e1.getRegistered(), e2.getRegistered()))
-					.collect(Collectors.toList());
+									.sorted((e1, e2) -> Integer.compare(e1.getCasual()+e1.getRegistered(), e2.getCasual()+e2.getRegistered()))
+									.collect(Collectors.toList());
 			// In descending order
 			Collections.reverse(sorted);
 
 			// Get top five days
 			List<BikeEntry> topFiveDays = sorted.subList(0, 5);
-			topFiveDays.forEach(tf -> System.out.println(tf.getRegistered()));
 			String[] POSITION = { "highest", "second highest", "third highest", "fourth highest", "fifth highest" };
 			String[] WEATHER = { "Clear, Few clouds, Partly cloudy, Partly cloudy",
 					"Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist",
@@ -58,7 +56,7 @@ public class Main {
 				String season = Utilities.toSeason(be.getSeason());
 				String day = Utilities.toWeekday(be.getWeekday());
 				String month = Utilities.toMonth(be.getMonth());
-				int total = be.getRegistered();
+				int total = be.getRegistered() + be.getCasual();
 				String weather = WEATHER[be.getWeather() - 1];
 				String holiday = (be.isHoliday()) ? "a holiday" : "not a holiday";
 				System.out.printf(

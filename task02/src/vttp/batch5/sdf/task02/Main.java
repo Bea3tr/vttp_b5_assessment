cd7043dedd17839d07b952a2ff9acbb8b0a38918
Beatrice <beatricesim31@gmail.com>
2024-10-30 13:56:14 +0800

    clean file

--- task02/src/vttp/batch5/sdf/task02/Main.java
+++ task02/src/vttp/batch5/sdf/task02/Main.java
@@ -1,9 +1,14 @@
 package vttp.batch5.sdf.task02;
 
 import java.io.*;
+import java.sql.Blob;
 import java.util.*;
 
 public class Main {
+
+	private static String PLAYER = "X";
+	private static String OPPONENT = "O";
+	private static String BLANK = ".";
 
 	public static String[][] tttboard(String file) throws Exception {
 		// Read file and create board
@@ -32,7 +37,7 @@ public class Main {
 		List<int[]> emptyPos = new ArrayList<>();
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
-				if (board[i][j].equals(".")) {
+				if (board[i][j].equals(BLANK)) {
 					int[] pos = { i, j };
 					emptyPos.add(pos);
 				}
@@ -47,76 +52,71 @@ public class Main {
 		// Check rows
 		for (int row = 0; row < board.length; row++) {
 			if (board[row][0].equals(board[row][1]) && board[row][1].equals(board[row][2])) {
-				if (board[row][0].equals("X")){
+				if (board[row][0].equals(PLAYER))
 					XWin = true;
-				}
-					
 			}
 			else {
 				int countO = 0;
 				int countS = 0;
 				for (int col = 0; col < board[0].length; col++) {
-					if (board[row][col].equals("O"))
+					if (board[row][col].equals(OPPONENT))
 						countO++;
-					else if (board[row][col].equals("."))
+					else if (board[row][col].equals(BLANK))
 						countS++;
 				}
-				if (countO == 2 && countS == 1){
+				if (countO == 2 && countS == 1)
 					OWin = true;
-				}
 			}
 		}
 
 		// Check columns
 		for (int col = 0; col < board[0].length; col++) {
-			int countO = 0;
-			int countS = 0;
 			if (board[0][col].equals(board[1][col]) && board[1][col].equals(board[2][col])) {
-				if (board[0][col].equals("X")){
+				if (board[0][col].equals(PLAYER)){
 					XWin = true;
 				}
 			} else {
-				
+				int countO = 0;
+				int countS = 0;
 				for (int row = 0; row < board.length; row++) {
-					if (board[row][col].equals("O"))
+					if (board[row][col].equals(OPPONENT))
 						countO++;
-					else if (board[row][col].equals("."))
+					else if (board[row][col].equals(BLANK))
 						countS++;
 				}
-				if (countO == 2 && countS == 1) {
+				if (countO == 2 && countS == 1) 
 					OWin = true;
-				}
-					
 			}
 		}
 
 		// Check diagonals
 		if ((board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2])) ||
 				(board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]))) {
-			if (board[1][1].equals("X"))
+			if (board[1][1].equals(PLAYER))
 				XWin = true;
 		}
 		else {
 			int countO = 0;
 			int countS = 0;
 			for(int i = 0; i < board.length; i++) {
-				if(board[i][i].equals("O"))
+				if(board[i][i].equals(OPPONENT))
 					countO++;
-				else if (board[i][i].equals("."))
+				else if (board[i][i].equals(BLANK))
 					countS++;
 			}
 			if (countO == 2 && countS == 1)
 					OWin = true;
 			else {
+				// Reset counts
 				countO = 0;
 				countS = 0;
 			}	
 			for(int i = board.length - 1; i >= 0; i--) {
 				for(int j = 0; j < board[0].length; j++) {
-					if(board[i][j].equals("O"))
-					countO++;
-				else if (board[i][j].equals("."))
-					countS++;
+					if(board[i][j].equals(OPPONENT))
+						countO++;
+					else if (board[i][j].equals(BLANK))
+						countS++;
 				}
 			}
 			if (countO == 2 && countS == 1)
@@ -135,7 +135,7 @@ public class Main {
 		int movesLeft = 0;
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
-				if (board[i][j].equals("."))
+				if (board[i][j].equals(BLANK))
 					movesLeft++;
 			}
 		}
@@ -152,7 +152,7 @@ public class Main {
 	}
 
 	public static int getUtility(String[][] board, int[] pos) {
-		board[pos[0]][pos[1]] = "X";
+		board[pos[0]][pos[1]] = PLAYER;
 		int utility = evaluateBoard(board);
 		return utility;
 	}
@@ -173,7 +173,7 @@ public class Main {
 		List<int[]> pos = getEmptyPos(board);
 		for(int[] p : pos) {
 			int utility = getUtility(board, p);
-			board[p[0]][p[1]] = ".";
+			board[p[0]][p[1]] = BLANK;
 			System.out.printf("y=%d, x=%d, utility=%d\n", p[0], p[1], utility);
 		}
 	}
